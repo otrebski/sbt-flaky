581eca425d3f36e63c4a40c58e8cdb98f02fdebf
Krzysztof <krzysztof.otrebski@gmail.com>
2017-05-24 22:55:01 +0200

    History trends of all aggregated test in Html report #20

--- src/main/scala/flaky/history/HistoryReport.scala
+++ src/main/scala/flaky/history/HistoryReport.scala
@@ -18,20 +18,6 @@ case class TestSummary(test: Test, stat: Stat)
 
 case class HistoryStat(test: Test, stats: List[Stat] = List.empty[Stat])
 
-case class Grouped(
-                    good: List[HistoryStat] = List.empty[HistoryStat],
-                    newCases: List[HistoryStat] = List.empty[HistoryStat],
-                    fixed: List[HistoryStat] = List.empty[HistoryStat],
-                    better: List[HistoryStat] = List.empty[HistoryStat],
-                    noChange: List[HistoryStat] = List.empty[HistoryStat],
-                    worse: List[HistoryStat] = List.empty[HistoryStat]) {
-
-  def all(): List[HistoryStat] = {
-    good ::: newCases ::: fixed ::: better ::: noChange ::: worse
-  }
-
-}
-
 case class HistoryReport(project: String, date: String, historicalRuns: List[HistoricalRun]) {
 
   def testSummary(historicalRuns: List[HistoricalRun]): List[TestSummary] = {
@@ -60,49 +46,4 @@ case class HistoryReport(project: String, date: String, historicalRuns: List[His
       }.toList.sortBy(_.test.test)
   }
 
-  def grouped(): Grouped = {
-    HistoryReport.grouped(testSummary(historicalRuns))
-  }
-}
-
-object HistoryReport {
-
-  def grouped(testsSummary: List[TestSummary]): Grouped = {
-    testsSummary
-      .groupBy(_.test)
-      .foldLeft(Grouped()) { (group, tl) =>
-        val test = tl._1
-        val stats = tl._2.sortBy(_.stat.date).map(_.stat)
-        HistoryReport.groupTestResult(group, test, stats)
-      }
-  }
-
-  def groupTestResult(group: Grouped, test: Test, stats: List[Stat]): Grouped = {
-    if (stats.size > 1) {
-      val last2 = stats.drop(stats.size - 2)
-      val ratePrevious = last2.head.failureRate()
-      val last = last2(1).failureRate()
-      if (!stats.exists(p = _.failureRate() > 0)) {
-        group.copy(good = HistoryStat(test, stats) :: group.good)
-      } else if (ratePrevious == 0 && last > 0) {
-        group.copy(newCases = HistoryStat(test, stats) :: group.newCases)
-      } else if (ratePrevious > last && last == 0) {
-        group.copy(fixed = HistoryStat(test, stats) :: group.fixed)
-      } else if (ratePrevious > last) {
-        group.copy(better = HistoryStat(test, stats) :: group.better)
-      } else if (ratePrevious < last) {
-        group.copy(worse = HistoryStat(test, stats) :: group.worse)
-      } else {
-        group.copy(noChange = HistoryStat(test, stats) :: group.noChange)
-      }
-    } else {
-      stats.headOption.map { firstStat =>
-        if (firstStat.failureRate() > 0) {
-          group.copy(newCases = HistoryStat(test, stats) :: group.newCases)
-        } else {
-          group.copy(good = HistoryStat(test, stats) :: group.good)
-        }
-      }.getOrElse(group)
-    }
-  }
 }
